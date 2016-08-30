cf18105ea367e5f083cd19ff141a27dd1f7aa441
maadhavi <maadhavidevops@gmail.com>
2016-08-30 15:25:34 +0530

    Added 1st script

---
+++ firstscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+##This is simple Hello world program
+clear
+
+echo "Hello World"
+
+cat << EOF
+###################################################################
+################Welcome to GIT Training world######################
+###################################################################
+EOF
+exit 0
